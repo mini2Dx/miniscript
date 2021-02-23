ece789d888f86fd5b87cb15da1adffa4d3cf2f9e
Thomas Cashman <thomascashman404@gmail.com>
2021-02-23 19:18:51 +0000

    Fix script skipping in KavaThreadPoolProvider

--- core/src/main/java/org/mini2Dx/miniscript/core/threadpool/KavaThreadPoolProvider.java
+++ core/src/main/java/org/mini2Dx/miniscript/core/threadpool/KavaThreadPoolProvider.java
@@ -1,5 +1,25 @@
 /**
- * Copyright 2020 Viridian Software Ltd.
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2021 Thomas Cashman
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
  */
 package org.mini2Dx.miniscript.core.threadpool;
 
@@ -11,17 +31,20 @@ import java.util.concurrent.ScheduledFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ConsoleThreadPoolProvider implements Runnable, ThreadPoolProvider {
+/**
+ * {@link ThreadPoolProvider} implementation for Kava-based runtimes
+ */
+public class KavaThreadPoolProvider implements Runnable, ThreadPoolProvider {
 	private final AtomicBoolean running = new AtomicBoolean(true);
 	private final Thread [] threads;
 
 	private final ReadWritePriorityQueue<ScheduledTask> scheduledTaskQueue = new ReadWritePriorityQueue<>();
 
-	public ConsoleThreadPoolProvider() {
+	public KavaThreadPoolProvider() {
 		this(Runtime.getRuntime().availableProcessors() + 1);
 	}
 
-	public ConsoleThreadPoolProvider(int maxConcurrentScripts) {
+	public KavaThreadPoolProvider(int maxConcurrentScripts) {
 		threads = new Thread[maxConcurrentScripts];
 		for(int i = 0; i < maxConcurrentScripts; i++) {
 			threads[i] = new Thread(this);
@@ -52,8 +75,6 @@ public class ConsoleThreadPoolProvider implements Runnable, ThreadPoolProvider {
 	@Override
 	public ScheduledFuture<?> schedule(Runnable command, long delay, TimeUnit unit) {
 		final ScheduledTask scheduledTask = ScheduledTask.allocate(command, System.nanoTime() + unit.toNanos(delay));
-		scheduledTaskQueue.offer(scheduledTask);
-
 		final ScheduledTaskFuture future = new ScheduledTaskFuture(scheduledTaskQueue);
 
 		scheduledTask.setFuture(future);
